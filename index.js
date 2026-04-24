575bfd761b30c0d7d27581838ffedb08974e0f6c
victorg430 <victorgichuhi15@gmail.com>
2026-04-24 21:41:55 +0300

    course-3-module-4-external-apis-and-fetching-lab

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+async function fetchWeather(state) {
+    const url = `https://api.weather.gov/alerts/active?area=${state}`;
+    
+    try{
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Failed to fetch weather data");
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        displayError(error.message);
+    }    
+}
